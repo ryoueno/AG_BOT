d3635eaf4f4d39389bed7e3d38c2c848a1d08a47
uenoryo <r.r.ag0611@outlook.jp>
2016-12-15 16:06:51 +0900

    Set base system of responce only bot.

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
 <?php
 echo "hello heroku";
 error_log(date('H:i:s'));
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+
+$app = new Silex\Application();
+
+$app->post('/callback', function (Request $request) use ($app) {
+    $client = new GuzzleHttp\Client();
+
+    $body = json_decode($request->getContent(), true);
+    foreach ($body['result'] as $msg) {
+        if (!preg_match('/(ぬるぽ|ヌルポ|ﾇﾙﾎﾟ|nullpo)/i', $msg['content']['text'])) {
+            continue;
+        }
+
+        $resContent = $msg['content'];
+        $resContent['text'] = 'ｶﾞｯ';
+
+        $requestOptions = [
+            'body' => json_encode([
+                'to' => [$msg['content']['from']],
+                'toChannel' => 1383378250, # Fixed value
+                'eventType' => '138311608800106203', # Fixed value
+                'content' => $resContent,
+            ]),
+            'headers' => [
+                'Content-Type' => 'application/json; charset=UTF-8',
+                'X-Line-ChannelID' => '1489137629',
+                'X-Line-ChannelSecret' => 'e74799cad8fb491e03d0891ffb9913f8',
+            ],
+            'proxy' => [
+                'https' => getenv('FIXIE_URL'),
+            ],
+        ];
+
+        try {
+            $client->request('post', 'https://trialbot-api.line.me/v1/events', $requestOptions);
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
+    }
+
+    return 'OK';
+});
+
+$app->run();
